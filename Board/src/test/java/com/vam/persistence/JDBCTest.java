db92d15b05d7181766fd963cfb25532a73a0a05c
JungWonjin <catman@khan.co.kr>
2024-12-16 12:01:04 +0900

    init commit

---
+++ Board/src/test/java/com/vam/persistence/JDBCTest.java
@@ -0,0 +1,11 @@
+package com.vam.persistence;
+
+public class JDBCTest {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
