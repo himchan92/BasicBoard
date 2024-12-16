c24cff56dbb1240df21952eccc17babb87e02b7d
JungWonjin <catman@khan.co.kr>
2024-12-16 17:31:41 +0900

    Model, service 설정, 테스트 반영

---
+++ Board/src/main/java/com/vam/model/BoardVO.java
@@ -0,0 +1,73 @@
+package com.vam.model;
+
+import java.sql.Date;
+
+public class BoardVO {
+	private int bno; //번호
+	
+	private String title; //제목
+	
+	private String content; //내용
+	
+	private String writer; //작가
+	
+	private Date regdate;
+	
+	private Date updateDate;
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", updateDate=" + updateDate + "]";
+	}
+	
+	
+}
---
+++ Board/src/main/java/com/vam/service/BoardService.java
@@ -0,0 +1,9 @@
+package com.vam.service;
+
+import com.vam.model.BoardVO;
+
+public interface BoardService {
+
+	//등록
+	public void enroll(BoardVO board);
+}
